8349b8588c2332289d33ef83855b1b0900bad63f
Ben Shaw <ben@beneboy.co.nz>
2013-09-11 11:44:37 +1200

    Working setup for 1.0b1

--- setup.py
+++ setup.py
@@ -1,10 +1,52 @@
+import os
 from distutils.core import setup
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join)
+    in a platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, package_data = [], {}
+
+
+for dirpath, dirnames, filenames in os.walk('helio'):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
+    if '__init__.py' in filenames:
+        packages.append(package_name)
+    elif filenames:
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.append(parts.pop())
+        relative_path.reverse()
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames if not f.startswith('.')])
 
 
 setup(
     name='helio',
     version='1.0b1',
-    py_modules=['helio'],
+    packages=packages,
+    package_data=package_data,
+    url='https://github.com/yellowpagesgroup/Helio',
+    author='Yellow Pages Group',
+    author_email='ben.shaw@yellow.co.nz',
     requires=[
         'coverage',
         'mock',
@@ -13,7 +55,6 @@ setup(
     ],
     classifiers=[
         'Environment :: Web Environment',
-        'Framework :: Helio',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
